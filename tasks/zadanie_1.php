63d3ca4c2f3dcc0afb71e13b4bd7ff93b64fa78e
Kinga Horniak <kitfa6@gmail.com>
2025-10-23 17:18:47 +0200

    Zadanie 1

--- tasks/zadanie_1.php
+++ tasks/zadanie_1.php
@@ -9,3 +9,11 @@ To jest dolar: $
 A to jest cudzysłów połączony z slashem: "\"
 
 */
+
+$a = "Pisze w języku PHP";
+$b = "To jest dolar: /$";
+$c = 'A to jest cudzysłów połączony z slashem: "\"';
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
