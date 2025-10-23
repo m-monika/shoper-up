143e5cd22ed3ffe51ac20600247f0eb9f413f29c
kkajstura <kkajstura.r2a@gmail.com>
2025-10-23 17:33:26 +0200

    Zadanie1

--- tasks/zadanie_1.php
+++ tasks/zadanie_1.php
@@ -1,11 +1,9 @@
 <?php
 
-/*
+$a= "Piszę w języku PHP";
+$b= "To jest dolar: '/$'";
+$c= "A to jest cudzysłów połączony z slashem: '\'";
 
-Napisz program, który wyświetli na ekranie::
-
-Piszę w języku PHP
-To jest dolar: $
-A to jest cudzysłów połączony z slashem: "\"
-
-*/
+var_dump($a);
+var_dump($b);
+var_dump($c);
