d8011754e858ecc313d985e0d801c5f945061312
pwalczak418 <pwalczak418@gmail.com>
2025-10-23 17:19:54 +0200

    Zadanie 1 - only one var used

--- tasks/zadanie_1.php
+++ tasks/zadanie_1.php
@@ -10,5 +10,9 @@ A to jest cudzysłów połączony z slashem: "\"
 
 */
 
-echo "Piszę w języku PHP\nTo jest dolar: $\n";
-echo 'A to jest cudzysłów połączony z slashem: "\"';
+$message = "Piszę w języku PHP\nTo jest dolar: $\n".'A to jest cudzysłów połączony z slashem: "\"';
+
+echo $message;
+
+#echo "Piszę w języku PHP\nTo jest dolar: $\n";
+#echo 'A to jest cudzysłów połączony z slashem: "\"';
