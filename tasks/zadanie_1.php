6bb534693447f627d5071946909a9b85333b218a
K Bolenbach <kbolenbach@gmail.com>
2025-10-23 17:13:40 +0200

    Zadanie 1

--- tasks/zadanie_1.php
+++ tasks/zadanie_1.php
@@ -9,3 +9,10 @@ To jest dolar: $
 A to jest cudzysłów połączony z slashem: "\"
 
 */
+
+$a = "Piszę w języku PHP";
+var_dump($a);
+$b = 'to jest dolar: /$';
+var_dump($b);
+$c = 'A to jest cudzysłów połączony z slashem: "\"';
+var_dump($c);
