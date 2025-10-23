2b248cf08451e2645467e20ab6a1ae3bf18140b8
Kinga Horniak <kitfa6@gmail.com>
2025-10-23 17:52:59 +0200

    Zadanie 2

--- tasks/zadanie_2.php
+++ tasks/zadanie_2.php
@@ -11,3 +11,14 @@ Napisz program, który wyświetli na ekranie:
 *********
 
 */
+
+$a = "    *\n";
+$b = "   ***\n";
+$c = "  *****\n";
+$d = " *******\n";
+$e = "*********";
+
+
+
+
+echo $a.$b.$c.$d.$e;
