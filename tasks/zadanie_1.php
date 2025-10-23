24f0345ac235d3ed20e60c3d52032ef6adb796b8
Daniel Kania <kania.daniel98@gmail.com>
2025-10-23 17:30:30 +0200

    Zadanie 1

--- tasks/zadanie_1.php
+++ tasks/zadanie_1.php
@@ -10,8 +10,8 @@ A to jest cudzysłów połączony z slashem: "\"
 
 */
 
-echo "Piszę w języku PHP\";
-echo "To jest dolar: \$\";
-echo "A to jest cudzysłów połączony z slashem: ;
+echo "Piszę w języku PHP\n";
+echo "To jest dolar: \$\n";
+echo "A to jest cudzysłów połączony z slashem:'/'" ;
 
 ?>
