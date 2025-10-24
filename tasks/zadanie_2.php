871816a594237c71713def11a6d49f0ef34eb2c9
K Bolenbach <kbolenbach@gmail.com>
2025-10-24 16:44:22 +0200

    zadanie 2

--- tasks/zadanie_2.php
+++ tasks/zadanie_2.php
@@ -12,7 +12,5 @@ Napisz program, który wyświetli na ekranie:
 
 */
 
-echo "<pre>\n";
-echo ("    *\n   ***\n  *****\n *******\n*********");
-echo "</pre>";
+echo ("    *<br>   ***<br>  *****<br> *******<br>*********");
 
