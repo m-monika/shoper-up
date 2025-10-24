34ff10119ac1357b31cfdb2051ed2b2912b5e9a7
K Bolenbach <kbolenbach@gmail.com>
2025-10-24 17:02:02 +0200

    zadanie 2

--- tasks/zadanie_2.php
+++ tasks/zadanie_2.php
@@ -12,5 +12,11 @@ Napisz program, który wyświetli na ekranie:
 
 */
 
-echo ("    *<br>   ***<br>  *****<br> *******<br>*********");
+$a = "    *";
+$b = "   ***";
+$c = "  *****";
+$d = " *******";
+$e = "*********";
+$f = "\n";
 
+echo $a . $f . $b . $f . $c . $f . $d . $f . $e . $f;
