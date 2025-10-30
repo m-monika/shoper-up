2c5fbcb63bdc7d2c164567a93e3ce14cac081955
Gabriela Zborowska <gabzborowskaa@gmail.com>
2025-10-30 17:03:56 +0100

    Zadanie 3

--- tasks/zadanie_3.php
+++ tasks/zadanie_3.php
@@ -2,7 +2,7 @@
 $text = "ShoperUp! Programowanie PHP - Lekcja 3";
 
 echo mb_strtoupper($text)  . "\n"; 
-echo mb_lcfirst($text)  . "\n";
+echo lcfirst($text)  . "\n";
 echo mb_substr($text, 10, 17)  . "\n"; 
 echo str_replace(' ', '', $text)  . "\n";
 echo strrev($text)  . "\n";
