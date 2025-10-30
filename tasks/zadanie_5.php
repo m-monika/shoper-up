ea5bfe2a588a8bddb831aa225c7dcb24db7386fe
Kinga Horniak <kitfa6@gmail.com>
2025-10-30 18:06:26 +0100

    Zadanie 5

--- tasks/zadanie_5.php
+++ tasks/zadanie_5.php
@@ -23,6 +23,6 @@ $givenMoneyByClient = 10000;
 $costOfProducts = 8515;
 
 $rest = (int)(($givenMoneyByClient-$costOfProducts)/100); // TODO
-$restPennies = $rest%100; // TODO
+$restPennies = ($givenMoneyByClient-$costOfProducts)%100; // TODO
 
 echo "Kasjerka powinna oddać klientowi: {$rest}zł {$restPennies} groszy.";
