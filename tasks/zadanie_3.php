5b710e710a82e77b5bcf452bb9bf3f2adba5d110
Gabriela Zborowska <gabzborowskaa@gmail.com>
2025-10-30 16:54:46 +0100

    Zadanie 3

--- tasks/zadanie_3.php
+++ tasks/zadanie_3.php
@@ -1,20 +1,9 @@
 <?php
-
-/*
-
-https://www.php.net/manual/en/book.strings.php
-
-Mamy do dyspozycji poniższą zmienną $text
-Używając poznanych funkcji operujących na stringach wyświetl na ekran poniższy tekst:
-
-SHOPERUP! PROGRAMOWANIE PHP - LEKCJA 3
-shoperUp! Programowanie PHP - Lekcja 3
-Programowanie PHP
-ShoperUp!ProgramowaniePHP-Lekcja3
-3 ajckeL - PHP einawomargorP !pUrepohS
-38
-
-*/
-
-
 $text = "ShoperUp! Programowanie PHP - Lekcja 3";
+
+echo mb_strtoupper($text)  . "\n"; 
+echo mb_lcfirst($text)  . "\n";
+echo mb_substr($text, 10, 17)  . "\n"; 
+echo str_replace(' ', '', $text)  . "\n";
+echo strrev($text)  . "\n";
+echo mb_strlen($text)  . "\n";
