69ed0a02488af63f889963c7c37a950e03b798fb
Daniel Kania <kania.daniel98@gmail.com>
2025-10-30 16:37:37 +0100

    zadanie 3

--- tasks/zadanie_1.php
+++ tasks/zadanie_1.php
@@ -12,6 +12,10 @@ A to jest cudzysłów połączony z slashem: "\"
 
 echo "Piszę w języku PHP\n";
 echo "To jest dolar: \$\n";
+<<<<<<< HEAD
 echo 'A to jest cudzysłów połączony z slashem:"\"' ;
+=======
+echo 'A to jest cudzysłów połączony z slashem: "\"' ;
+>>>>>>> 2b3af1d68574fa20aedef5b8e4434a8d3c1c45ad
 
 ?>
--- tasks/zadanie_3.php
+++ tasks/zadanie_3.php
@@ -1,11 +1,10 @@
 <?php
+$text = "ShoperUp! Programowanie PHP - Lekcja 3";
 
-$age = 25;
+echo strtoupper ($text) . "\n";
+echo strtolower ($text) . "\n";
+echo substr($text, 10, 17)  . "\n";
+//echo str_replace("ShoperUp! Programowanie PHP - Lekcja 3",$text)  . "\n"; 
+echo strrev($text)  . "\n";
+echo mb_strlen($text)  . "\n";
 
-/*
-
-Użyj zmiennej $age tak aby wyświetlić poniższy tekst:
-
-Kasia ma 25 lat.
-
-*/
