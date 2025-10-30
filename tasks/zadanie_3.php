e18c488d6ae7b19ca0b38c19d3318378a115e095
Daniel Kania <kania.daniel98@gmail.com>
2025-10-30 17:24:56 +0100

    Zadanie 3 poprawka

--- tasks/zadanie_3.php
+++ tasks/zadanie_3.php
@@ -20,7 +20,7 @@ ShoperUp!ProgramowaniePHP-Lekcja3
 $text = "ShoperUp! Programowanie PHP - Lekcja 3";
 
 echo strtoupper ($text) . "\n";
-echo strtolower ($text) . "\n";
+echo lcfirst ($text) . "\n";
 echo substr($text, 10, 17)  . "\n";
 echo str_replace(" ", "", $text) . "\n"; 
 echo strrev($text)  . "\n";
