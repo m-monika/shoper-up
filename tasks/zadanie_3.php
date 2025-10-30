2cb04c832459f28fc05ff89fb08de64101556e99
K Bolenbach <kbolenbach@gmail.com>
2025-10-30 20:49:45 +0100

    Zadanie 3

--- tasks/zadanie_3.php
+++ tasks/zadanie_3.php
@@ -20,7 +20,7 @@ $text = "ShoperUp! Programowanie PHP - Lekcja 3";
 
 echo strtoupper($text) . "\n";
 echo lcfirst($text) . "\n";
-echo substr($text, 9,18) . "\n";
+echo substr($text, 10,18) . "\n";
 echo str_replace(" ", "", $text) . "\n";
 echo strrev($text) ."\n";
 echo strlen($text);
