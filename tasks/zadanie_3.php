4bbc04e48d93613320367e05156c7f488d2ef603
kkajstura <kkajstura.r2a@gmail.com>
2025-10-30 17:01:11 +0100

    Zadanie3

--- tasks/zadanie_3.php
+++ tasks/zadanie_3.php
@@ -7,4 +7,4 @@ echo lcfirst($text) . "\n";
 echo substr($text, 10, 17) . "\n";
 echo str_replace(" ", "", $text) . "\n";
 echo strrev ($text) . "\n";
-echo str_replace("ShoperUp! Programowanie PHP - Lekcja 3","38", $text) . "\n";
+echo strlen($text) . "\n";
