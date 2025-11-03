38285a66863b8d587361155bad318c7d46c0537f
Monika Młodzik <m852.monika@gmail.com>
2025-11-03 21:43:49 +0100

    Task 7 - added description

--- tasks/zadanie_7.php
+++ tasks/zadanie_7.php
@@ -2,17 +2,16 @@
 
 /*
 
-TODO
+Napisz instrukcje warunkowe które sprawdzą który klient wydał więcej w sklepie
+Jeśli klient A wydał więcej, wyświetl na ekranie komunikat "Klient A wydał więcej"
+Jeśli kliet A i B wydali tyle samo: "Klient A wydał tyle samo co klient B"
+Jeśli klient A wydał mniej niż klient B: "Klient B wydał więcej"
 
 */
 
-$customerA = $params[0];
-$customerB = $params[1];
+$customerA = $params[0]; // tej linijki nie ruszamy :)
+$customerB = $params[1]; // tej linijki nie ruszamy :)
 
-if ($customerA > $customerB) {
-    echo "Klient A wydał więcej";
-} elseif ($customerA == $customerB) {
-    echo "Klient A wydał tyle samo co klient B";
-} else {
-    echo "Klient B wydał więcej";
-}
+// ...
+
+echo "Klient A wydał więcej";
