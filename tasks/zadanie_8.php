3a8c9e99fb51b840bbb80428d1d05bc5fb4c53a6
Kamila Filiks <kamila.filiks.kf@gmail.com>
2025-11-06 17:34:23 +0100

    Zadanie 8

--- tasks/zadanie_8.php
+++ tasks/zadanie_8.php
@@ -14,5 +14,8 @@ $role = $params[0]; // tej linijki nie ruszamy :)
 $isLogged = $params[1]; // tej linijki nie ruszamy :)
 
 // ...
-
+if ($role == "admin" && $isLogged == true){
 echo "Dostęp przyznany";
+} else {
+    echo "Brak dostępu";
+}
