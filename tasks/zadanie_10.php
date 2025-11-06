292534c68351d6f95f9e51a90cc4716c316b830a
kkajstura <kkajstura.r2a@gmail.com>
2025-11-06 18:16:19 +0100

    Zadanie10

--- tasks/zadanie_10.php
+++ tasks/zadanie_10.php
@@ -1,21 +1,16 @@
 <?php
 
-/*
-
-Twoim zadaniem jest wyliczenie rabatu dla klientów.
-Mamy typ klienta pod zmienną $type, może ona przyjmować wartości "silver" / "gold" / "none".
-Jeśli klient jest typu "gold" dostaje rabat 20%.
-Jeśli klient jest typu "silver" dostaje rabat 10%.
-Jeśli klient jest typu "none" nie dostaje rabatu.
-
-Rabat nalicza się tylko jeśli łączna kwota zakupów przekracza 100zł. 
-Pod zmienną $productsCost znajduje się cena w złotówkach.
-
-*/
-
 $productsCost = $params[0]; // tej linijki nie ruszamy :)
 $type = $params[1]; // tej linijki nie ruszamy :)
 
 $discountAmount = 0;
+
+if ($productsCost > 100 && $type === "gold") {
+    $discountAmount = $productsCost * 0.2;
+} elseif ($productsCost > 100 && $type === "silver") {
+    $discountAmount = $productsCost * 0.1;
+}else {
+    $discountAmount = 0;
+}
 
 echo "Rabat wynosi: {$discountAmount}zł";
