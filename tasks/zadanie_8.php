6ac22b46d00049ba6e7c1a28ef1c4e847fa9b5a9
kkajstura <kkajstura.r2a@gmail.com>
2025-11-06 18:26:27 +0100

    Zadanie8

--- tasks/zadanie_8.php
+++ tasks/zadanie_8.php
@@ -3,7 +3,7 @@
 $role = $params[0]; // tej linijki nie ruszamy :)
 $isLogged = $params[1]; // tej linijki nie ruszamy :)
 
-if ($role === "admin" && ($isLogged === "true" || $isLogged === true)) {
+if ($role === "admin" && $isLogged === true) {
 echo "Dostęp przyznany";
 } else {
     echo "Brak dostępu";
