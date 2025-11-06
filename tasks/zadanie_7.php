02479cb19f95e4e043fcfa713ebab308f71de5a9
Kinga Horniak <kitfa6@gmail.com>
2025-11-06 16:31:14 +0100

    Zadanie 7

--- tasks/zadanie_7.php
+++ tasks/zadanie_7.php
@@ -12,6 +12,15 @@ Jeśli klient A wydał mniej niż klient B: "Klient B wydał więcej"
 $customerA = $params[0]; // tej linijki nie ruszamy :)
 $customerB = $params[1]; // tej linijki nie ruszamy :)
 
-// ...
 
-echo "Klient A wydał więcej";
+if ($customerA > $customerB){
+    echo "Klient A wydał więcej".PHP_EOL;
+}
+elseif ($customerA == $customerB){
+     echo "Klient A wydał tyle samo co klient B".PHP_EOL;
+}
+else{
+    echo "Klient B wydał więcej".PHP_EOL;
+}
+
+
