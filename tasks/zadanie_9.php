fcada8fe1221c66031e3a622de8c5f0e6c59891c
kkajstura <kkajstura.r2a@gmail.com>
2025-11-06 17:41:38 +0100

    Zadanie9

--- tasks/zadanie_9.php
+++ tasks/zadanie_9.php
@@ -1,20 +1,10 @@
 <?php
 
-/*
-
-Napisz skrypt, który sprawdza, czy użytkownik może wziąć udział w konkursie.
-Warunki udziału:
-- wiek ≥ 18
-- użytkownik zaznaczył zgodę na przetwarzanie danych ($consent = true)
-
-Jeśli warunki są spełnione → "Możesz wziąć udział"
-Jeśli nie → "Nie spełniasz warunków"
-
-*/
-
 $consent = $params[0]; // tej linijki nie ruszamy :)
 $age = $params[1]; // tej linijki nie ruszamy :)
 
-// ...
-
+if ($age >= "18" && $consent === true) {
 echo "Możesz wziąć udział";
+} else {
+    echo "Nie spełniasz warunków";
+}
