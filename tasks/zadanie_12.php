7d756bc715d5ee2c3c2746b6c6480cd0361875e2
K Bolenbach <kbolenbach@gmail.com>
2025-11-13 17:40:39 +0100

    zadanie 12

--- tasks/zadanie_12.php
+++ tasks/zadanie_12.php
@@ -13,12 +13,12 @@ Dla number = 5:
 
 $number = $params[0]; // tej linijki nie ruszamy :)
 
-for ($i = 1; $i <= 5; $i++) {
+for ($i = $number; $i <= 1; $i--) {
     if ($number >= 2) {
     echo '' . PHP_EOL;
     break;
     } else {
-    echo $number . PHP_EOL;
+    echo $number . ' ';
     }
     $number++;
 }
