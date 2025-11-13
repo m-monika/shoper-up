fc56601094f8ce06a49c8341411f55db5cfaf408
Daniel Kania <kania.daniel98@gmail.com>
2025-11-13 19:00:34 +0100

    zadanie12.3
    zadanie12.3

--- tasks/zadanie_12.php
+++ tasks/zadanie_12.php
@@ -2,7 +2,7 @@
 
 /*
 
-Napisz program,, 
+Napisz program,,
 który dla zmiennej $number za pomocą pętli for wypisuje liczby od $number do 1.
 
 Przykład:
@@ -17,3 +17,4 @@ for ($i = $number; $i >= 1; $i--) {
     echo $i . " ";
 }
 ?>
+
