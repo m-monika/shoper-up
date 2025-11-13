d4e1a1ef6f66ed42599296c62001a5f7be76008e
Daniel Kania <kania.daniel98@gmail.com>
2025-11-13 19:15:24 +0100

    Zadanie 12-zacznij w końcu swiecic sie na zielonoooo

--- tasks/zadanie_12.php
+++ tasks/zadanie_12.php
@@ -1,18 +1,5 @@
 <?php
-
-/*
-
-Napisz program,,
-który dla zmiennej $number za pomocą pętli for wypisuje liczby od $number do 1.
-
-Przykład:
-Dla number = 5:
-5 4 3 2 1
-
-*/
-
-$number = $params[0]; // tej linnijki nie ruszamy :)
-
+$number = $params[0];
 for ($i = $number; $i >= 1; $i--) {
     echo $i . " ";
 }
