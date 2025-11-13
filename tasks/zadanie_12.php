d06d3fe4b9cadea011ac5638b7ff6f72928bab50
kkajstura <kkajstura.r2a@gmail.com>
2025-11-13 16:38:56 +0100

    Zadanie12

--- tasks/zadanie_12.php
+++ tasks/zadanie_12.php
@@ -2,10 +2,6 @@
 
 $number = $params[0]; // tej linijki nie ruszamy :)
 
-if ($number > 1)
-for ($number; $number >= 1; $number--) {
+if ($number > 0)
+for ($number; $number >= 1; $number--)
  echo  $number . " ";
-}else{
-    for ($number; $number <= 1; $number++)
- echo  $number . " ";
-    }
