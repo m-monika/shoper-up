9781665ce9fff3661ac378ef0f6cbdd9d806fa0b
Daniel Kania <kania.daniel98@gmail.com>
2025-11-13 19:39:48 +0100

    ??

--- tasks/zadanie_12.php
+++ tasks/zadanie_12.php
@@ -1,10 +1,11 @@
 <?php
-
 $number = $params[0]; // tej linijki nie ruszamy :)
 
-for ($i = $number; $i >= 1; $i--) {
-    echo $i;
-    if ($i > 1) {
-        echo " ";
+if (is_numeric($number) && $number > 0) {
+    for ($i = $number; $i >= 1; $i--) {
+        echo $i;
+        if ($i > 1) {
+            echo " ";
+        }
     }
 }
