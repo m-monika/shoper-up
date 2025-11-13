85803986d56b3b31c0ff6f89068d61897e40ddc0
Monika Młodzik <monika.mlodzik@shoper.pl>
2025-11-13 17:53:34 +0100

    fix

--- tasks/zadanie_12.php
+++ tasks/zadanie_12.php
@@ -1,1 +1,18 @@
-<?php/*Napisz program, który dla zmiennej $number za pomocą pętli for wypisuje liczby od $number do 1.Przykład:Dla number = 5:5 4 3 2 1*/$number = $params[0]; // tej linijki nie ruszamy :)for ($i = $number; $i >= 1; $i--) {    echo $i . " ";}
+<?php
+
+/*
+
+Napisz program, 
+który dla zmiennej $number za pomocą pętli for wypisuje liczby od $number do 1.
+
+Przykład:
+Dla number = 5:
+5 4 3 2 1
+
+*/
+
+$number = $params[0]; // tej linijki nie ruszamy :)
+
+for ($i = $number; $i >= 1; $i--) {
+    echo $i . " ";
+}
