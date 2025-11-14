fe46338211526d508a736138b266ae548a05515f
Monika Młodzik <monika.mlodzik@shoper.pl>
2025-11-14 14:57:18 +0100

    Zadanie 12

--- tasks/zadanie_12.php
+++ tasks/zadanie_12.php
@@ -1,17 +1,12 @@
 <?php
 
-/*
-
-Napisz program, 
-który dla zmiennej $number za pomocą pętli for wypisuje liczby od $number do 1.
-
-Przykład:
-Dla number = 5:
-5 4 3 2 1
-
-*/
-
 $number = $params[0]; // tej linijki nie ruszamy :)
 
-echo "5 4 3 2 1";
-
+if ($number > 0) {
+    for ($i = $number; $i >= 1; $i--) {
+        echo $i;
+        if ($i > 1) {
+            echo " ";
+        }
+    }
+}
