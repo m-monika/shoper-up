c681fa22819043da10dff0a711707440f35fdaa0
Monika Młodzik <monika.mlodzik@shoper.pl>
2025-11-14 15:01:52 +0100

    Zadanie 12

--- tasks/zadanie_12.php
+++ tasks/zadanie_12.php
@@ -15,3 +15,11 @@ $number = $params[0]; // tej linijki nie ruszamy :)
 
 echo "5 4 3 2 1";
 
+if ($number > 0) {
+    for ($i = $number; $i >= 1; $i--) {
+        echo $i;
+        if ($i > 1) {
+            echo " ";
+        }
+    }
+}
