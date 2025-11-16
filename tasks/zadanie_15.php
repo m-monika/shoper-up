90a7fa25c2accc4af88da0718c2cdf84cb24ed6b
Kinga Horniak <kitfa6@gmail.com>
2025-11-16 12:16:48 +0100

    Zadanie 15

--- tasks/zadanie_15.php
+++ tasks/zadanie_15.php
@@ -4,9 +4,14 @@
 
 Pod zmienną $shops masz tablicę sklep - cena dla wybranego produktu:
 [
-  ["store" => "Sklep A", "price" => 1200],
-  ["store" => "Sklep B", "price" => 1250],
-  ["store" => "Sklep C", "price" => 1190]
+  ["store" => "Sklep A", 
+  "price" => 1200],
+
+  ["store" => "Sklep B",
+   "price" => 1250],
+
+  ["store" => "Sklep C", 
+  "price" => 1190]
 ]
 
 Zadanie:
@@ -17,4 +22,25 @@ Najlepsza oferta: Sklep C — 1190 zł
 
 $shops = $params[0]; // tej linijki nie ruszamy :)
 
-echo "Najlepsza oferta: Sklep C — 1190 zł";
+$lowestprice = 10000000;
+
+foreach ($shops as $key => $value){
+	
+  if (is_array($value)){
+    
+    $store = "" ;
+    $price = 0 ;
+
+    foreach ($value as $key2 => $value2){
+
+      if ($key2 == "price"){
+        $price = $value2;
+        }elseif($key2 == "store"){
+      	$store = $value2;
+        }
+      } if ($lowestprice > $price){
+  	$lowestprice = $price;
+  	$shopname = $store;
+      }
+    } 
+  }echo "Najlepsza oferta: " . $shopname . " - " . $lowestprice . " zł" ;
