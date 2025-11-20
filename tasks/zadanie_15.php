05addcd7dbef2627f8f86d0c0a4a7c0b640476f8
K Bolenbach <kbolenbach@gmail.com>
2025-11-20 13:09:54 +0100

    Zadanie 15

--- tasks/zadanie_15.php
+++ tasks/zadanie_15.php
@@ -21,7 +21,7 @@ foreach ($shops as $store => $price) {
   $shopsCount = count($shops);
     for ($i = 0; $i < $shopsCount; $i++) {
       for ($j = $i + 1; $j < $shopsCount; $j++ ) {
-        if ($shops[$i]['price'] < $shops[$j]['price']) {
+        if ($shops[$i]['price'] > $shops[$j]['price']) {
           //nie chcemy zgubić i
           $temporary = $shops[$i];
           //skoro i jest mniejsze niz j, to j ma byc wyzej na liscie sortowania
@@ -34,4 +34,4 @@ foreach ($shops as $store => $price) {
     }
 }
 
-echo "Najlepsza oferta: " . $shops[0]['store'] . " - " . $shops[0]['price'] . "zł";
+echo "Najlepsza oferta: " . $shops[0]['store'] . " — " . $shops[0]['price'] . " zł";
