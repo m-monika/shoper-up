bd39d7cf10384a8cad3698f1a1f4a2c2c551c178
K Bolenbach <kbolenbach@gmail.com>
2025-11-20 20:21:02 +0100

    Zadanie15

--- tasks/zadanie_15.php
+++ tasks/zadanie_15.php
@@ -18,20 +18,13 @@ Najlepsza oferta: Sklep C — 1190 zł
 $shops = $params[0]; // tej linijki nie ruszamy :)
 
 foreach ($shops as $store => $price) {
-  $shopsCount = count($shops);
-    for ($i = 0; $i < $shopsCount; $i++) {
-      for ($j = $i + 1; $j < $shopsCount; $j++ ) {
-        if ($shops[$i]['price'] > $shops[$j]['price']) {
-          //nie chcemy zgubić i
-          $temporary = $shops[$i];
-          //skoro i jest mniejsze niz j, to j ma byc wyzej na liscie sortowania
-          $shops[$i] = $shops[$j];
-          //a w miejsce i idzie trzymana wartosc w j, bo j była większa
-          $shops[$j] = $temporary;
-          
-        }
+  $minPrice = null;
+  $shopName = null;
+  foreach ($shops as $storeInfo) {
+      if ($minPrice === null || $minPrice > $storeInfo['price']){
+           $minPrice = $storeInfo['price'];
+           $shopName = $storeInfo['store'];
       }
-    }
+  }
 }
-
-echo "Najlepsza oferta: " . $shops[0]['store'] . " — " . $shops[0]['price'] . " zł";
+echo "Najlepsza oferta: " . $shopName . " — " . $minPrice . " zł";
