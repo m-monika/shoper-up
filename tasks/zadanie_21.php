b3f108ddf9c5e0fd9862cfa70480279307f25b2f
Gabriela Zborowska <gabzborowskaa@gmail.com>
2025-11-22 16:15:31 +0100

    Zadanie 21

--- tasks/zadanie_21.php
+++ tasks/zadanie_21.php
@@ -1,46 +1,27 @@
 <?php
 
-/*
-Pod zmienną $products mamy tablicę produktów.
-Napisz program, który wyświetli informację o ilości produktów oraz listę produktów posortowaną w zależności od wybranej opcji:
-- name: po nazwie
-- price: po cenie
-
-sortowanie może być rosnące lub malejące:
-- asc: rosnąco
-- desc: malejąco
-
-Jeśli któryś z parametrów do sortowania ($key, $sort) nie został podany, wyświetlamy produkty tak, jak zostały podane.
-Jeśli któryś z parametrów do sortowania ($key, $sort) nie pasuje do zdefiniowanych, wyświetlamy informację o błędzie: Nieprawidłowy parametr.
-
-Zmienne:
-$key - klucz, po którym sortujemy (name/price)
-$sort - rodzaj sortowania (asc/desc)
-
-Przykład:
-
-$products = [
-    'Laptop' => 3000,
-    'Monitor' => 2500,
-    'Klawiatura' => 300,
-    'Mysz' => 250,
-    'Słuchawki' => 700,
-];
-
-$key = 'name;
-$sort = 'asc';
-
-Wyświetl wszystko w formacie:
-
-Liczba produktów: 5
-Klawiatura | 300 zł
-Laptop | 3000 zł
-Monitor | 2500 zł
-Mysz | 250 zł
-Słuchawki | 700 zł
-
-*/
-
 $products = $params[0]; // tej linijki nie ruszamy :)
 $key = $params[1]; // tej linijki nie ruszamy :)
 $sort = $params[2]; // tej linijki nie ruszamy :)
+
+$validKey = ['name', 'price'];
+$validSort = ['asc', 'desc'];
+$isSorted = false;
+
+if (empty($key) || empty($sort)) {
+	
+} elseif (!in_array($key, $validKey) || !in_array($sort, $validSort)) {
+    echo 'Nieprawidłowy parametr.\n';
+    exit;
+} else {
+	if ($key === 'name') {
+        ($sort === 'asc') ? ksort($products) : krsort($products);
+    } elseif ($key === 'price') {
+        ($sort === 'asc') ? asort($products) : arsort($products);
+    }
+}
+
+echo 'Liczba produktów: ' . count($products) . PHP_EOL;
+foreach ($products as $name => $price) {
+    echo $name . " | " . $price . " zł\n";
+}
