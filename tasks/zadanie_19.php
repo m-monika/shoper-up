7946545a9164ca002a3024469df92d54ad966c47
Gabriela Zborowska <gabzborowskaa@gmail.com>
2025-11-22 14:52:17 +0100

    Zadanie 19

--- tasks/zadanie_19.php
+++ tasks/zadanie_19.php
@@ -1,36 +1,44 @@
 <?php
 
-/*
-Napisz program, który oblicza koszt dostawy w zależności od wybranej metody dostawy i sumy zamówienia. Użyj instrukcji switch.
-
-Zmienne:
-$sum - suma zamówienia
-$shipping - metoda dostawy
-
-Koszty dostawy:
-- paczkomat: 9 zł
-- kurier: 15 zł
-- sklep: 0 zł
-- poczta: 20 zł
-
-Jeśli zostanie wybrana inna metoda dostawy, program wyświetla komunikat:
-Nieprawidłowa metoda dostawy.
-
-Wysyłka jest darmowa, jeśli:
-- wybrana metoda to 'paczkomat' i suma zamówienia przekracza 39 zł
-- wybrana metoda to 'kurier' i suma zamówienia przekracza 49 zł
-
-Jeśli wysyłka jest darmowa wyświetlamy komunikat:
-Koszt dostawy wynosi 0 zł.
-
-Przykład:
-
-$sum = 20;
-$shipping = 'paczkomat';
-
-Koszt dostawy wynosi 9 zł.
-
-*/
-
 $sum = $params[0]; // tej linijki nie ruszamy :)
 $shipping = $params[1]; // tej linijki nie ruszamy :)
+
+$freePaczkomat = 39;
+$freeKurier = 49;
+$shippingCost = null;
+$paczkomatCost = 9;
+$kurierCost = 15;
+$sklepCost = 0;
+$pocztaCost = 20;
+
+switch ($shipping) {
+	case 'paczkomat':
+		if ($sum > $freePaczkomat) {
+			$shippingCost = 0;
+		} else {
+            $shippingCost = $paczkomatCost;
+        } 
+		break;
+	case 'kurier':
+		if ($sum > $freeKurier) {
+			$shippingCost = 0;
+		} else {
+			$shippingCost = $kurierCost;
+		}
+		break;
+	case 'sklep':
+		$shippingCost = $sklepCost;
+		break;
+	case 'poczta':
+		$shippingCost = $pocztaCost;
+		break;
+	default:
+		echo 'Nieprawidłowa metoda dostawy.';
+		break;
+}
+
+if ($shippingCost === 0) {
+	echo "Koszt dostawy wynosi 0 zł.\n";
+} elseif ($shippingCost !== null) {
+	echo "Koszt dostawy wynosi " . $shippingCost . " zł.\n";
+}
