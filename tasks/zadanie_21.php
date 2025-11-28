f621a9c48ed26b870f77ade9e49500d488c17f6e
Jacek Guzdek <oldskullowy2@gmail.com>
2025-11-28 23:46:29 +0100

    Zadanie 21

--- tasks/zadanie_21.php
+++ tasks/zadanie_21.php
@@ -41,6 +41,40 @@ Słuchawki | 700 zł
 
 */
 
+
 $products = $params[0]; // tej linijki nie ruszamy :)
 $key = $params[1]; // tej linijki nie ruszamy :)
 $sort = $params[2]; // tej linijki nie ruszamy :)
+
+
+if ((isset($key) && ($key !== 'name' && $key !== 'price')) 
+    || (isset($sort) && ($sort !== 'asc' && $sort !== 'desc'))){
+    echo "Nieprawidłowy parametr.";
+}else{
+    switch($key){
+        case 'name':
+            if($sort === 'desc'){
+                krsort($products);
+            }elseif($sort ==='asc'){
+                ksort($products);
+            }
+            break;
+        case 'price':
+            if($sort === 'desc'){
+                arsort($products);
+            }elseif($sort ==='asc'){
+                asort($products);
+            }
+            break;
+        }
+    $quanity = 0;
+    foreach ($products as $list){
+        $quanity++; 
+    }
+    echo "Liczba produktów: " . $quanity . "\n";    
+
+    foreach ($products as $listName => $listPrice){
+        echo $listName . ' | ' . $listPrice . " zł" . "\n";
+            }
+}
+
