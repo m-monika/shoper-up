e5829ec7e0bd7778049b700ffc1ef7fb3baf5f8b
Gabriela Zborowska <gabzborowskaa@gmail.com>
2025-12-04 18:15:53 +0100

    Zadanie 23

--- tasks/zadanie_23.php
+++ tasks/zadanie_23.php
@@ -29,3 +29,33 @@ Kod niepoprawny
 */
 
 $postcodes = $params[0]; // tej linijki nie ruszamy :)
+
+function isValidPostcode(string $postcode): bool 
+{
+	if (strlen($postcode) !== 6) {
+        return false;
+    }
+    
+    if (substr($postcode, 2, 1) !== '-') {
+        return false;
+    }
+    
+	$part1 = substr($postcode, 0, 2);
+    if (!is_numeric($part1)) {
+        return false;
+    }
+
+    $part2 = substr($postcode, 3, 3);
+    if (!is_numeric($part2)) {
+        return false;
+    }    
+    return true;
+}
+
+foreach ($postcodes as $postcode) {
+    if (isValidPostcode($postcode)) {
+        echo "Kod poprawny\n";
+    } else {
+        echo "Kod niepoprawny\n";
+    }
+}
