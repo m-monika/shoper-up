0eb162380138d3bc198603fe0e8bc76af7233700
K Bolenbach <kbolenbach@gmail.com>
2025-12-04 21:16:02 +0100

    Zadanie 22

--- tasks/zadanie_22.php
+++ tasks/zadanie_22.php
@@ -31,3 +31,18 @@ Witaj Jacek, sprawdź naszą ofertę! Odwiedź nasz sklep!
 */
 
 $clients = $params[0]; // tej linijki nie ruszamy :)
+
+function isVip (array $client): string
+{
+        $name = $client['name'];
+
+        if (!empty($client['vip']) && $client['vip'] === true) {
+            return "Cześć $name! Mamy super ofertę specjalnie dla klientów VIP! Odwiedź nasz sklep!" . PHP_EOL;
+        } else {
+            return "Witaj $name, sprawdź naszą ofertę! Odwiedź nasz sklep!" . PHP_EOL;
+        }
+}
+
+foreach ($clients as $client) {
+    echo isVip($client);
+}
