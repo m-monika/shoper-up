1b4878ddcdd09e0515139877abb899f03b470d09
Tomasz Mazur <tomasz.mazur@shoper.pl>
2025-12-09 15:03:43 +0100

    lekcja 8 - zadania

---
+++ tasks/zadanie_27.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+Otrzymujesz tablicę asocjacyjną zawierającą dane nowego użytkownika systemu.
+Twoim zadaniem jest normalizacja tych danych oraz wykonanie walidacji przy użyciu funkcji wbudowanych i własnych.
+
+Normalizacja:
+- username - usuwamy białe znaki z początku i końca (trim)
+- email - usuwamy białe znaki z początku i końca, małe litery
+
+Walidacja:
+- username - min. 3 znaki
+- email - poprawny adres email (filter_var)
+- password - min. 10 znaków, podane 2 razy (password1, password2), musi się zgadzać
+
+Dane wejściowe:
+array $newUser - tablica asocjacyjna zawierająca dane nowego użytkownika (email, username, password1, password2)
+
+Przykład:
+$newUser = [
+    'email' => '  x@SHOPER.PL',
+    'username' => 'SuperUSER',
+    'password1' => 'admin12345',
+    'password2' => 'admin12345',
+];
+
+SuperUSER (x@shoper.pl) został zarejestrowany
+
+$newUser = [
+    'email' => '  x@SHOPER.PL',
+    'username' => 'X',
+    'password1' => 'admin12345',
+    'password2' => '12345admin',
+];
+
+Niepoprawne dane
+
+*/
+
+//var_dump($params);die;
+$newUser = $params[0]; // tej linijki nie ruszamy :)
---
+++ tasks/zadanie_28.php
@@ -0,0 +1,116 @@
+<?php
+
+/*
+Otrzymujesz JSON-a z danymi klientów z systemu X, które trzeba zaimportować do systemu Y.
+Twoim zadaniem jest przetworzenie tej listy, normalizacja danych (naprawienie błędów formatowania), walidacja i odrzucenie rekordów, które są niekompletne.
+Musisz też wygenerować raport końcowy, który powie, ilu użytkowników udało się zaimportować, a ilu odrzucono i dlaczego.
+Raport powinien być w formacie json.
+
+Normalizacja:
+- first_name, last_name - usuwamy białe znaki z początku i końca, pierwsza litera wielka, pozostałe litery małe
+- email - usuwamy białe znaki z początku i końca, małe litery
+- phone - zaczyna się od +48, potem same cyfry
+- type - w systemie X to int, w systemie Y to string (standard, vip), musimy zmienić stringa na inta:
+    1 -> standard
+    2 -> vip
+    każda inna wartość lub brak wartości -> standard
+
+Walidacja:
+- first_name, last_name - min. 2 znaki
+- email - poprawny adres email (filter_var)
+- phone - zaczyna się od +48, następnie 9 cyfr
+- type - standard lub vip
+w przypadku niepoprawnej walidacji zwracamy komunikat "Invalid field"
+
+Dane wejściowe:
+string $clientsJson- string w formacie json zawierający dane klientów do zaimportowania
+
+Przykład:
+[
+  {
+    "first_name": "  ADAM ",
+    "last_name": "Nowak",
+    "email": "Adam.Nowak@example.com",
+    "phone": "500-123-456"
+  },
+  {
+    "first_name": "Ewa",
+    "last_name": "",
+    "email": "bledny-email",
+    "phone": "600123456",
+    "type": 1
+  },
+  {
+    "first_name": "Piotr",
+    "last_name": "Kowalski",
+    "email": "piotr.k@example.com",
+    "phone": "+48 700 123 456",
+    "type": 2
+  }
+]
+przekazany w formie:
+$clientsJson = '[{"first_name":"  ADAM ","last_name":"Nowak","email":"Adam.Nowak@example.com","phone":"500-123-456"},{"first_name":"Ewa","last_name":"","email":"bledny-email","phone":"600123456","type":1},{"first_name":"Piotr","last_name":"Kowalski","email":"piotr.k@example.com","phone":"+48 700 123 456","type":2},{"first_name":"Pawe\u0142","last_name":"NOWAK ","email":"pawelnowakk@example.com","phone":"+90 700 123 456","type":1}]';
+
+Oczekiwany rezultat (raport) o następującej strukturze (json):
+{
+  "summary": {
+    "total": 4,
+    "success": 2,
+    "error": 2
+  },
+  "details": [
+    {
+      "status": "success",
+      "data": {
+        "first_name": "Adam",
+        "last_name": "Nowak",
+        "email": "adam.nowak@example.com",
+        "phone": "+48500123456",
+        "type": "standard"
+      }
+    },
+    {
+      "status": "error",
+      "errors": {
+        "email": "Invalid field",
+        "last_name": "Invalid field"
+      },
+      "data": {
+        "first_name": "Ewa",
+        "last_name": "",
+        "email": "bledny-email",
+        "phone": "600123456",
+        "type": 1
+      }
+    },
+    {
+      "status": "success",
+      "data": {
+        "first_name": "Piotr",
+        "last_name": "Kowalski",
+        "email": "piotr.k@example.com",
+        "phone": "+48700123456",
+        "type": "vip"
+      }
+    },
+    {
+      "status": "error",
+      "errors": {
+        "phone": "Invalid field"
+      },
+      "data": {
+        "first_name": "Paweł",
+        "last_name": "NOWAK ",
+        "email": "pawelnowakk@example.com",
+        "phone": "+90 700 123 456",
+        "type": 1
+      }
+    }
+  ]
+}
+
+zwrócony w formie:
+{"summary":{"total":4,"success":2,"error":2},"details":[{"status":"success","data":{"first_name":"Adam","last_name":"Nowak","email":"adam.nowak@example.com","phone":"+48500123456","type":"standard"}},{"status":"error","errors":{"email":"Invalid field","last_name":"Invalid field"},"data":{"first_name":"Ewa","last_name":"","email":"bledny-email","phone":"600123456","type":1}},{"status":"success","data":{"first_name":"Piotr","last_name":"Kowalski","email":"piotr.k@example.com","phone":"+48700123456","type":"vip"}},{"status":"error","errors":{"phone":"Invalid field"},"data":{"first_name":"Pawe\u0142","last_name":"NOWAK ","email":"pawelnowakk@example.com","phone":"+90 700 123 456","type":1}}]}
+*/
+
+$clientsJson = $params[0]; // tej linijki nie ruszamy :)
