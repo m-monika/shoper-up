dbe2f047b656d8917b1173d28f1f7036dbcd4f87
Gabriela Zborowska <gabzborowskaa@gmail.com>
2025-12-09 21:16:22 +0100

    Zadanie 26

--- tasks/zadanie_26.php
+++ tasks/zadanie_26.php
@@ -1,41 +1,41 @@
 <?php
 
-/*
-Napisz program który wygeneruje paragon dla podanego koszyka produktów
-
-Zmienne:
-$cart - tablica reprezentująca koszyk z produktami
-
-
-Przykład:
-
-$cart = [
-    [
-        'name' => 'Chleb',
-        'price' => 800,
-        'qty' => 2,
-    ],
-    [
-        'name' => 'Mleko',
-        'price' => 400,
-        'qty' => 3,
-    ],
-    [
-        'name' => 'Czekolada',
-        'price' => 500,
-        'qty' => 1,
-    ]
-];
-
-Wynik:
-
---- TWOJE ZAKUPY ---
-2x Chleb ... 8,00 PLN
-3x Mleko ... 4,00 PLN
-1x Czekolada ... 5,00 PLN
---------------------
-DO ZAPŁATY: 33,00
---------------------
-*/
-
 $cart = $params[0]; // tej linijki nie ruszamy :)
+
+$totalPriceGr = 0;
+
+$receiptLines = [];
+
+foreach ($cart as $item) {
+	$name = $item['name'];
+    $priceUnitGr = $item['price']; //w groszach
+    $qty = $item['qty'];
+	
+	$productTotalGr = $priceUnitGr * $qty;
+	$totalPriceGr += $productTotalGr; //dalej w groszach
+	
+	$productTotalZl = $productTotalGr / 100; //w koncu zł
+	$formattedPrice = number_format($productTotalZl,  2, ',', '');
+	
+	$line = sprintf(
+        "%dx %s ... ", 
+        $qty, 
+        $name
+    );
+    
+    $receiptLines[] = $line . $formattedPrice . ' PLN';
+}
+
+echo "--- TWOJE ZAKUPY ---" . PHP_EOL;
+
+foreach ($receiptLines as $line) {
+    echo $line . "\n";
+}
+
+echo  "--------------------" . PHP_EOL;
+
+$finalPriceZl = $totalPriceGr / 100;
+
+$formattedFinalPrice = number_format($finalPriceZl, 2, ',', '');
+
+echo "DO ZAPŁATY: " . $formattedFinalPrice . PHP_EOL . "--------------------";
