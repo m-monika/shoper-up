9ce18f19490560bdedd48f41d88a23bfe8bed817
pwalczak418 <pwalczak418@gmail.com>
2025-12-12 21:10:50 +0100

    Task 28

--- tasks/zadanie_28.php
+++ tasks/zadanie_28.php
@@ -18,7 +18,7 @@ Normalizacja:
 Walidacja:
 - first_name, last_name - min. 2 znaki
 - email - poprawny adres email (filter_var)
-- phone - zaczyna się od +48, następnie 9 cyfr
+- phone - zaczyna się od +48, następnie 9 cyfr (dokleić +48 jak nie ma)
 - type - standard lub vip
 w przypadku niepoprawnej walidacji zwracamy komunikat "Invalid field"
 
@@ -119,3 +119,168 @@ zwrócony w formie:
 */
 
 $clientsJson = $params[0]; // tej linijki nie ruszamy :)
+
+// this function process received JSON data, normalize it, verify it and then returning report in json string //
+
+function processApiCall(string $reveicedJsonString): string {
+
+  // functions //
+
+  // normalize provided data entry and return updated array
+  function normalizeData(array $dataArray): array {
+
+    if (!isset($dataArray['type']) || $dataArray['type'] == 1) {
+      $type = "standard";
+    } else {
+      $type = "vip";
+    }
+
+    if (function_exists('nameNormalize') == false) {
+    function nameNormalize(string $name): string {
+      $name = trim($name);
+      $name = strtolower($name);
+      $name = ucfirst($name);
+      return $name;
+    }
+    }
+
+    $first_name = nameNormalize($dataArray['first_name']);
+    $last_name = nameNormalize($dataArray['last_name']);
+
+    $email = trim($dataArray['email']);
+    $email = strtolower($email);
+
+    if (function_exists('phoneNormalize') == false) {
+      function phoneNormalize(string $phone): string {
+
+        $specialChars = array("-", " ", ".");
+        $phone = str_replace($specialChars, "", $phone);
+
+        if (str_starts_with($phone, "+48") && strlen($phone) == 12 && is_numeric(substr($phone, -11, 11))) {
+          return $phone;
+        } elseif (strlen($phone) == 9 && is_numeric($phone)) {
+          return '+48' . $phone;
+        } else { 
+          return "not_valid";
+        }
+      }
+      }
+
+      $phone = phoneNormalize($dataArray['phone']);
+
+      $dataArray = [
+        'first_name' => $first_name,
+        'last_name' => $last_name,
+        'email' => $email,
+        'phone' => $phone,
+        'type' => $type
+      ];
+
+      return $dataArray;
+  }
+
+  // validates the data entry and return array with status & data
+  function validateData(array $dataArray, array $dataArrayOriginal): array {
+    $d = $dataArray;
+    $do = $dataArrayOriginal;
+
+    $statusFlag = true;
+
+    $details = [
+      'status' => '',
+      'errors' => [],
+      'data' => [ 
+        'first_name' => $d['first_name'],
+        'last_name' => $d['last_name'],
+        'email' => $d['email'],
+        'phone' => $d['phone'],
+        'type' => $d['type']
+      ]
+    ];
+
+    if (strlen($d['first_name']) < 2) {
+      $details['errors']['first_name'] = 'Invalid field';
+      $statusFlag = false;
+    }
+    // email field is not in order (!)
+    if (!filter_var($d['email'], FILTER_VALIDATE_EMAIL)) {
+      $details['errors']['email'] = 'Invalid field';
+      $statusFlag = false;
+    }
+    if (strlen($d['last_name']) < 2) {
+      $details['errors']['last_name'] = 'Invalid field';
+      $statusFlag = false;
+    }
+    if ($d['phone'] == 'not_valid') {
+      $details['errors']['phone'] = 'Invalid field';
+      $statusFlag = false;
+    }
+    if ($d['type'] != 'standard' && $d['type'] != 'vip') {
+      $details['errors']['type'] = 'Invalid field';
+      $statusFlag = false;
+      }
+
+    if (!$statusFlag) {
+      $details['status'] = 'error';
+      // if there is any error with data, we need to pass original data into the raport
+      $details['data'] = [ 
+        'first_name' => $do['first_name'],
+        'last_name' => $do['last_name'],
+        'email' => $do['email'],
+        'phone' => $do['phone'],
+        'type' => $do['type']
+      ];
+    } elseif ($statusFlag) {
+      $details['status'] = 'success';
+      unset($details['errors']);
+    }
+
+    return $details;
+
+  }
+
+  // main //
+
+  $decodedArray = json_decode($reveicedJsonString, true);
+
+  $totalCount = 0;
+  $successCount = 0;
+  $errorCount = 0;
+
+  $raport = [
+    'summary' => [
+      'total' => 0,
+      'success' => 0,
+      'error' => 0
+    ],
+    'details' => []
+    ];
+
+  foreach ($decodedArray as $entry) {
+    $totalCount ++;
+    $givenArray = $entry;
+    $normalizedArray = normalizeData($entry);
+    
+    $entryRaport = validateData($normalizedArray, $givenArray);
+
+    if ($entryRaport['status'] == 'error') {
+      $errorCount ++;
+    } elseif ($entryRaport['status'] == 'success') {
+      $successCount ++;
+    }
+
+    $raport['details'][] = $entryRaport;
+
+  }
+
+  $raport['summary']['total'] = $totalCount;
+  $raport['summary']['success'] = $successCount;
+  $raport['summary']['error'] = $errorCount;
+
+  return json_encode($raport);
+
+}
+
+$apiResponse = processApiCall($clientsJson);
+
+echo $apiResponse;
