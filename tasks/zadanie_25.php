c1663c9d29d1b0993bdfeb7357d195bba1eccc08
K Bolenbach <kbolenbach@gmail.com>
2025-12-29 21:26:34 +0100

    zadanie 25

--- tasks/zadanie_25.php
+++ tasks/zadanie_25.php
@@ -48,3 +48,37 @@ Kabel HDMI: 30,00 zł
 
 $products = $params[0]; // tej linijki nie ruszamy :)
 $order = $params[1]; // tej linijki nie ruszamy :)
+
+
+if(function_exists('sortProducts') == false) {
+    function sortProducts (array $products, string $order): array {
+
+        if ($order == 'desc') {
+            usort($products, fn($firstProduct, $secondProduct) => $secondProduct['price'] <=> $firstProduct['price']);
+            return $products;
+        } elseif ($order == 'asc') {
+            usort($products, fn($firstProduct, $secondProduct) => $firstProduct['price'] <=> $secondProduct['price']);
+            return $products;
+        } elseif (empty($order)) {
+            echo 'Coś poszło nie tak.';
+        }
+    }
+}
+
+if(function_exists('formatPrice') == false) {
+    function formatPrice (int $productPrice): string {
+        return number_format($productPrice / 100, 2, ',', '');
+}
+}
+
+if(function_exists('showPriducts') == false) {
+    function showProducts (array $sortedProducts): string {
+        return $sortedProducts['name'] . ": " . formatPrice($sortedProducts['price']) . " zł" . PHP_EOL . "<br>";
+    }
+}
+
+$sortedProducts = sortProducts($products, $order);
+
+foreach ($sortedProducts as $product) {
+    echo showProducts($product);
+}
