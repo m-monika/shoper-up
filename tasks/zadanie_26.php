299bb90c4bb87a5abd9d6b47c84ad0be84215e72
K Bolenbach <kbolenbach@gmail.com>
2025-12-29 22:26:37 +0100

    Zadanie 26

--- tasks/zadanie_26.php
+++ tasks/zadanie_26.php
@@ -39,3 +39,44 @@ DO ZAPŁATY: 33,00
 */
 
 $cart = $params[0]; // tej linijki nie ruszamy :)
+
+if(function_exists('formatPrice') == false) {
+    function formatPrice (int $productPrice): string {
+        return number_format($productPrice / 100, 2, ',', '');
+}
+}
+
+if(function_exists('showProducts') == false) {
+    function showProducts (array $cartProductsList): string {
+        $listOfProducts = '';
+        foreach($cartProductsList as $product) {
+            $listOfProducts .= $product['qty'] . "x " . $product['name'] . " ... " . formatPrice($product['price']) . " PLN" . PHP_EOL;
+        }
+        return $listOfProducts;
+    }
+}
+
+if(function_exists('sumProducts') == false) {
+    function sumProducts (array $cartProductsList): int {
+        $cartPrice = 0;
+        foreach ($cartProductsList as $product) {
+            $cartPrice += $product['qty'] * $product['price'];
+        }
+        return $cartPrice;
+    }
+}
+
+if(function_exists('receiptPrint') == false) {
+    function receiptPrint (array $cartProducts): string {
+        $receiptBoilerplate = 
+        [
+            $title = '--- TWOJE ZAKUPY ---',
+            $divider = '--------------------',
+            $summary = 'DO ZAPŁATY: '
+        ];
+
+        return $title . PHP_EOL . showProducts($cartProducts) . $divider . PHP_EOL . $summary . formatPrice(sumProducts($cartProducts)) . PHP_EOL . $divider;
+    }
+}
+
+echo receiptPrint($cart);
