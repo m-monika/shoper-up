56e945d9ee0482c7a7fb3dede194afb41b024f61
kapakrystian <kapakrystian@gmail.com>
2026-01-13 21:39:35 +0100

    Zadanie 24

--- tasks/zadanie_24.php
+++ tasks/zadanie_24.php
@@ -46,59 +46,6 @@ $filterPrice = $params[1]; // tej linijki nie ruszamy :)
 $filterMode = $params[2]; // tej linijki nie ruszamy :)
 
 
-// function getFilteredProducts(array $products, float $filterPrice, string $filterMode): array
-// {
-//     if ($filterMode === 'gte') {
-
-//         $higherPrice = array_filter($products, function($product) use ($filterPrice){
-//         return $product['price'] >= $filterPrice;
-//         }, ARRAY_FILTER_USE_BOTH);
-
-//         return $higherPrice;
-
-//     } elseif ($filterMode === 'lte') {
-
-//         $lowerPrice = array_filter($products, function($product) use ($filterPrice){
-//         return $product['price'] <= $filterPrice;
-//         }, ARRAY_FILTER_USE_BOTH);
-
-//         return $lowerPrice;
-//     }
-// }
-
-// $results = getFilteredProducts($products, $filterPrice, $filterMode);
-
-// foreach ($results as $key => $result) {
-//     $decimalPrice = $result['price'] / 100;
-//     $formattedPrice = number_format($decimalPrice, 2, ',', '');
-//     echo "{$result['name']}: {$formattedPrice} zł\n";
-// }
-
-// if (!function_exists('getFilteredProducts')) {
-//     function getFilteredProducts(array $products, float $price, string $mode): array
-//     {
-//         if ($mode === 'gte') {
-//             return array_filter($products, function($product) use ($price) {
-//                 return $product['price'] >= $price;
-//             });
-//         } elseif ($mode === 'lte') {
-//             return array_filter($products, function($product) use ($price) {
-//                 return $product['price'] <= $price;
-//             });
-//         }
-//         return [];
-//     }
-// }
-
-// $results = getFilteredProducts($products, $filterPrice, $filterMode);
-
-// foreach ($results as $result) {
-//     $decimalPrice = $result['price'] / 100;
-//     $priceWithDot = sprintf("%0.2f", $decimalPrice);
-//     $formattedPrice = str_replace('.', ',', $priceWithDot);
-//     echo "{$result['name']}: {$formattedPrice} zł\n";
-// }
-
 if (!function_exists('getFilteredProducts')) {
     function getFilteredProducts(array $products, float $filterPrice, string $filterMode): array
     {
