7d476191f1eb7b25fa746e2f8628fc3d1ec8a5d4
Gabriela Zborowska <gabzborowskaa@gmail.com>
2026-01-14 19:45:07 +0100

    Zadanie 27

--- tasks/zadanie_27.php
+++ tasks/zadanie_27.php
@@ -1,40 +1,36 @@
 <?php
 
-/*
-Otrzymujesz tablicę asocjacyjną zawierającą dane nowego użytkownika systemu.
-Twoim zadaniem jest normalizacja tych danych oraz wykonanie walidacji przy użyciu funkcji wbudowanych i własnych.
-
-Normalizacja:
-- username - usuwamy białe znaki z początku i końca (trim)
-- email - usuwamy białe znaki z początku i końca, duże litery zamieniamy na małe
-
-Walidacja:
-- username - min. 3 znaki
-- email - poprawny adres email (filter_var)
-- password - min. 10 znaków, podane 2 razy (password1, password2), musi się zgadzać
-
-Dane wejściowe:
-array $newUser - tablica asocjacyjna zawierająca dane nowego użytkownika (email, username, password1, password2)
-
-Przykład:
-$newUser = [
-    'email' => '  x@SHOPER.PL',
-    'username' => 'SuperUSER',
-    'password1' => 'admin12345',
-    'password2' => 'admin12345',
-];
-
-SuperUSER (x@shoper.pl) został zarejestrowany
-
-$newUser = [
-    'email' => '  x@SHOPER.PL',
-    'username' => 'X',
-    'password1' => 'admin12345',
-    'password2' => '12345admin',
-];
-
-Niepoprawne dane
-
-*/
-
 $newUser = $params[0]; // tej linijki nie ruszamy :)
+
+//usunięcie białych znaków w loginie
+$newUser['username'] = trim($newUser['username']);
+//usuwanie bialych znaków i zamiana na małe litery w mailu
+$newUser['email'] = strtolower(trim($newUser['email']));
+
+$error = [];
+
+//sprawdzanie dlugosci loginu
+if (strlen($newUser['username']) < 3) {
+	$error[] = "Niepoprawne dane";
+}
+
+//sprawdzanie emalia
+if (!filter_var($newUser['email'], FILTER_VALIDATE_EMAIL)) {
+	$error[] = "Niepoprawne dane";
+}
+
+//sprawdzanie dlugości hasła
+if (strlen($newUser['password1']) < 10) {
+	$error[] = "Niepoprawne dane";
+}
+
+//sprawdzanie czy obydwa hasła są takie same
+if ($newUser['password1'] !== $newUser['password2']) {
+	$error[] = "Niepoprawne dane";
+}
+
+if (empty($error)) {
+	echo $newUser['username'] . ' (' . strtolower(trim($newUser['email'])) . ')' . ' został zarejestrowany';
+} else {
+	echo "Niepoprawne dane";
+}
