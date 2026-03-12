f523ab355d29a90aed456e8e512093e7efbe26e6
kapakrystian <kapakrystian@gmail.com>
2026-03-12 17:30:27 +0100

    Zadanie 0.2

---
+++ Basket.php
@@ -0,0 +1,19 @@
+<?php
+
+class Basket
+{
+    private array $products = [];
+    public function addProduct(Product $product): void
+    {
+        $this->products[] = $product;
+    }
+
+    public function getSum(): float
+    {
+        $sum = 0.0;
+        foreach ($this->products as $product) {
+            $sum += $product->getPrice();
+        }
+        return $sum;
+    }
+}
