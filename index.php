f9cfe83231115c10e2f273dad6050b6985bb2f93
Kinga Horniak <kitfa6@gmail.com>
2026-03-12 17:32:31 +0100

    Zadanie 0.2

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-echo "Hello World!!!!! TEST TEST";
+
+require_once('Basket.php');
+require_once('Product.php');
+
+
+
+$product1 = new Product("Laptop", 1500.00);
+$product2 = new Product("Smartphone", 500.00);
+$product3 = new Product("Headphones", 200.00);
+
+$basket = new Basket();
+$basket->addProduct($product1);
+$basket->addProduct($product2);
+$basket->addProduct($product3);
+echo "Total: " . $basket->getSum() . " PLN";
