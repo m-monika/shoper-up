9bd2a270b0eb6e16d9e4959758509b35452649f1
Gabriela Zborowska <gabzborowskaa@gmail.com>
2026-03-12 17:33:55 +0100

    Zadanie 0.2

--- index.php
+++ index.php
@@ -1,44 +1,7 @@
 <?php
 
-class Product
-{
-    private string $name;
-    private float $price;
-
-    public function __construct(string $name, float $price)
-    {
-        $this->name = $name;
-        $this->price = $price;
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function getPrice(): float
-    {
-        return $this->price;
-    }
-}
-
-class Basket
-{
-    private array $products = [];
-    public function addProduct(Product $product): void
-    {
-        $this->products[] = $product;
-    }
-
-    public function getSum(): float
-    {
-        $sum = 0.0;
-        foreach ($this->products as $product) {
-            $sum += $product->getPrice();
-        }
-        return $sum;
-    }
-}
+require_once('Product.php');
+require_once('Basket.php');
 
 $product1 = new Product("Laptop", 1500.00);
 $product2 = new Product("Smartphone", 500.00);
