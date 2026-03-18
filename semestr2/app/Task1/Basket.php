4f932fd52a1cc3168170a3ca69ad5f51f925a2d0
Kinga Horniak <kitfa6@gmail.com>
2026-03-18 20:03:12 +0100

    Zadanie 1

--- semestr2/app/Task1/Basket.php
+++ semestr2/app/Task1/Basket.php
@@ -6,12 +6,23 @@ namespace App\Task1;
 
 class Basket
 {
+    private array $products = [];
     public function addProduct(array $product): void
     {
+        $this->products[] = $product;
     }
 
     public function getSum(): float
     {
-        return 0.0;
+        $sum = 0.0;
+
+        foreach ($this->products as $product){
+            $price = $product['price'];
+            $qty = $product['qty'];
+
+            $sum += $price * $qty;
+        }
+
+        return $sum;
     }
 }
