b9e23795094fe6692ed16b7f2eccaefed7b36f90
Gabriela Zborowska <gabzborowskaa@gmail.com>
2026-03-18 19:13:16 +0100

    Zadanie 2

--- semestr2/app/Task2/Basket.php
+++ semestr2/app/Task2/Basket.php
@@ -2,16 +2,25 @@
 
 declare(strict_types=1);
 
+require_once 'Product.php';
+
 namespace App\Task2;
 
 class Basket
 {
+    private array $products = [];
+
     public function addProduct(Product $product): void
     {
+        $this->products[] = $product;
     }
 
     public function getSum(): float
     {
-        return 0.0;
+        $total = 0.0;
+        foreach ($this->items as $item) {
+            $total += (float)$item['price'] * $item['qty'];
+        }
+        return $total;
     }
 }
