d4d188bb6adfec8b410dbd2db1f64c80c8c2d383
Jacek <jacek@SHPR-L0614-Jacek-Guzdek.local>
2026-03-19 17:08:49 +0100

    Zadanie 0.4

---
+++ semestr2/app/PaymentApiClient.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+class PaymentApiClient
+{
+    
+    private string $apiKey = '';
+    private bool $valid = false;
+    public function __construct(string $apiKey) {
+
+    $this->apiKey=$apiKey;
+
+    if($apiKey=="superSekretnyKlucz"){
+        $this->valid=true;
+    }else{
+        $this->valid=false;
+    }
+
+    }
+    
+    public function createPayment(int $amount): void 
+    {  
+        if($this->valid==true){
+            echo "Utworzono płatność na kwotę: " . $amount . ",00 \n";
+        }else{
+            echo "Błąd połączenia";
+        }
+
+    }
+}
