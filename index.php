001a5b19bc634cc21e72f43c7dff72abc84fe0cd
kapakrystian <kapakrystian@gmail.com>
2026-03-19 17:30:12 +0100

    Zadanie 0.4

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-echo "Hello World!!! - zadanie 0";
+require 'semestr2/vendor/autoload.php';
+// require 'semestr2/app/PaymentApiClient.php';
+
+$apiClient = new App\PaymentApiClient('superSekretnyKlucz');
+
+$apiClient->createPayment(10000);
+$apiClient->createPayment(20000);
+$apiClient->createPayment(5000);
