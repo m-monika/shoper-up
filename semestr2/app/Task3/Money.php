806c7ce5bcb73139c32b28c61b56a4379e0296aa
K B <kbolenbach@gmail.com>
2026-03-25 19:56:59 +0100

    Zadanie 3

--- semestr2/app/Task3/Money.php
+++ semestr2/app/Task3/Money.php
@@ -6,8 +6,21 @@ namespace App\Task3;
 
 class Money
 {
+    public int $amount;
+    public string $currency;
+
+    public function __construct(int $amount = 0, string $currency = 'PLN')
+    {
+        $this->amount = $amount;
+        $this->currency = $currency;
+    }
+
+    public function getAmount(): float 
+    {
+        return $this->amount / 100;
+    }
     public function getFormatted(): string
     {
-        return '';
+        return number_format($this->getAmount(), 2, ',', '') . " " . $this->currency;
     }
 }
