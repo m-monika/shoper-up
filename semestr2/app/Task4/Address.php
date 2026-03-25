7a1f8495126307288a721846fa7800622e6777ce
K B <kbolenbach@gmail.com>
2026-03-25 20:47:26 +0100

    Zadanie 4

--- semestr2/app/Task4/Address.php
+++ semestr2/app/Task4/Address.php
@@ -6,8 +6,18 @@ namespace App\Task4;
 
 class Address
 {
+    private string $street;
+    private string $city;
+    private string $zipCode;
+
+    public function __construct(string $street, string $city, string $zipCode)
+    {
+        $this->street = $street;
+        $this->city = $city;
+        $this->zipCode = $zipCode;
+    }
     public function getFullAddress(): string
     {
-        return '';
+        return $this->street .", " . $this->zipCode . " " . $this->city;
     }
 }
