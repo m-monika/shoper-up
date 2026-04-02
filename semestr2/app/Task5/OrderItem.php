0ee6bad0ee97f592bd2fe5ca27cc1fd63e86f4d3
Jacek <jacek@SHPR-L0614-Jacek-Guzdek.local>
2026-04-02 17:14:42 +0200

    Zadanie 6

--- semestr2/app/Task5/OrderItem.php
+++ semestr2/app/Task5/OrderItem.php
@@ -6,7 +6,9 @@ namespace App\Task5;
 
 class OrderItem
 {
-    public function __construct(private string $productName = "", private int $quantity = 0, private int $price = 0){}
+    public function __construct(private string $productName = '', private int $quantity = 0, private int $price = 0)
+    {
+    }
     public function getTotalPrice(): int
     {
         return $this->price * $this->quantity;
