e80d0a45045305f78e501fdfff60fed9fda26f19
Gabriela Zborowska <gabzborowskaa@gmail.com>
2026-04-02 18:00:57 +0200

    Zadanie 7

--- semestr2/app/Task7/Discounts/PercentageDiscount.php
+++ semestr2/app/Task7/Discounts/PercentageDiscount.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Task7\Discounts;
 
-class PercentageDiscount
+class PercentageDiscount implements DiscountStrategyInterface
 {
     private float $percentage;
 
