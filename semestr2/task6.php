ff7bc7f4e529a7b0c1a32d83e6063e0d7ab21efd
pwalczak418 <pwalczak418@gmail.com>
2026-04-02 17:31:04 +0200

    Doślemy :)

--- semestr2/task6.php
+++ semestr2/task6.php
@@ -1,3 +1,13 @@
 <?php
+require 'vendor/autoload.php';
 
-echo "TODO";
+use App\Task6\Payment\PaymentGateway as Payment;
+use App\Task6\Shipping\ShippingService as Shipping;
+use App\Task6\OrderProcessor;
+
+$paymentGateway = new Payment("Przelew");
+$shippingService = new Shipping("OrlenPaczkaDziałająca");
+
+$processor = new OrderProcessor($paymentGateway, $shippingService);
+$result = $processor->processOrder('ORD-2026-001', 299.99, 1.5, 'ul. Pawia 9, Kraków');
+print_r($result);
