ec63a5698243e7037a8798177ffb9268deab6b0f
pwalczak418 <pwalczak418@gmail.com>
2026-04-06 18:19:44 +0200

    Zadanie 4

--- semestr2/app/Task4/Order.php
+++ semestr2/app/Task4/Order.php
@@ -6,8 +6,14 @@ namespace App\Task4;
 
 class Order
 {
+    public function __construct(private string $number, private Address $billingAddress, private Address $shippingAddress) {}
+
     public function isBillingSameAsShipping(): bool
     {
-        return false;
+        if ($this->billingAddress == $this->shippingAddress) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
