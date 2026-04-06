981556bf38f41a0d14404bbae7191220f906fe0b
pwalczak418 <pwalczak418@gmail.com>
2026-04-06 17:46:40 +0200

    Zadanie 3

--- semestr2/app/Task3/Money.php
+++ semestr2/app/Task3/Money.php
@@ -6,8 +6,12 @@ namespace App\Task3;
 
 class Money
 {
+    public function __construct(private int $amount, private string $currency = "PLN") {}
+    
     public function getFormatted(): string
     {
-        return '';
+        $convertedAmount = $this->amount * 0.01;
+        $valueStr = str_replace(".", ",", sprintf("%.2f", $convertedAmount));
+        return sprintf("%s %s", $valueStr, $this->currency);
     }
 }
