41da2fbd5ad005ce252718f119a9d63afb26a26e
pwalczak418 <pwalczak418@gmail.com>
2026-04-06 18:58:58 +0200

    Zadanie 5

--- semestr2/app/Task5/Order.php
+++ semestr2/app/Task5/Order.php
@@ -6,23 +6,37 @@ namespace App\Task5;
 
 class Order
 {
+
+    public function __construct(private string $number) {}
+
     private array $items = [];
 
     public function addItem(OrderItem $item): void
-    {}
-
-    public function getShippingCost(): int
     {
-        return 0;
+        $this->items[] = $item;
     }
 
     public function calculateItemsTotal(): int
     {
-        return 0;
+        $count = 0;
+        foreach ($this->items as $key => $value) {
+            $count = $count + $value->getTotalPrice();
+        }
+        return $count;
+    }
+
+    public function getShippingCost(): int
+    {
+        
+        if ($this->calculateItemsTotal() >= 15000) {
+            return 0;
+        } else {
+            return 1500;
+        }
     }
 
     public function calculateGrandTotal(): int
     {
-        return 0;
+        return $this->calculateItemsTotal() + $this->getShippingCost();
     }
 }
