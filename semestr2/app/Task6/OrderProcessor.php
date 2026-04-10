cdb8b52dcadeeff91f310069b1af2bcc63f5db5e
K B <kbolenbach@gmail.com>
2026-04-10 18:26:24 +0200

    Zadanie 6

--- semestr2/app/Task6/OrderProcessor.php
+++ semestr2/app/Task6/OrderProcessor.php
@@ -4,15 +4,29 @@ declare(strict_types=1);
 
 namespace App\Task6;
 
+use App\Task6\Payment\PaymentGateway;
+use App\Task6\Shipping\ShippingService;
+
+require 'semestr2/vendor/autoload.php';
+
 class OrderProcessor
 {
+    private object $payment;
+    private object $shipping;
+
+    public function __construct(PaymentGateway $payment, ShippingService $shipping)
+    {
+        $this->payment = $payment;
+        $this->shipping = $shipping;
+    }
+
     public function processOrder(string $orderNumber, float $amount, float $weight, string $address): array
     {
         return [
             'order' => $orderNumber,
-            'payment' => '',
-            'shipping_cost' => '',
-            'shipping' => '',
+            'payment' => $this->payment->processPayment($amount),
+            'shipping_cost' => $this->shipping->calculateCost($weight),
+            'shipping' => $this->shipping->ship($address),
         ];
     }
 }
