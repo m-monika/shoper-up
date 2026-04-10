94d297222831ca364df35a0ee352132af47bebbf
K B <kbolenbach@gmail.com>
2026-04-10 19:44:06 +0200

    Zadanie 6

--- semestr2/task6.php
+++ semestr2/task6.php
@@ -1,3 +1,14 @@
 <?php
 
-echo "TODO";
+require 'semestr2/vendor/autoload.php';
+
+use App\Task6\Payment\PaymentGateway;
+use App\Task6\Shipping\ShippingService;
+use App\Task6\OrderProcessor;
+
+$paymentGateway = new PaymentGateway('tpay');
+$shippingService = new ShippingService('kurier');
+
+$processor = new OrderProcessor($paymentGateway, $shippingService);
+$result = $processor->processOrder('ORD-2026-001', 299.99, 1.5, 'ul. Pawia 9, Kraków');
+print_r($result);
