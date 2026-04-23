6f3989546c192b4d3d50f8d7237b23fe22792907
pwalczak418 <pwalczak418@gmail.com>
2026-04-23 10:19:33 +0200

    Zadanie 10

--- semestr2/task10.php
+++ semestr2/task10.php
@@ -1,3 +1,23 @@
 <?php
+declare(strict_types=1);
+namespace App\Task10;
 
-echo "TODO";
+require 'vendor/autoload.php';
+
+use App\Task10\Product;
+use App\Task10\Basket;
+
+$produktTestowy = new Product("Test", 60.44);
+$innyProdukt = new Product("Inny", 90.22);
+$trzeciProdukt = new Product("Kolejny", 110.77);
+
+$basket1 = new Basket();
+$basket1->addProduct($produktTestowy);
+$basket1->addProduct($innyProdukt);
+$basket1->addProduct($trzeciProdukt);
+
+$loader = new \Twig\Loader\FilesystemLoader('templates/task10');
+
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('basket.html.twig', ['basket' => $basket1]);
