d5d17b5c2c810006b159723e2c349aa586dabf35
Daniel Kania <kania.daniel98@gmail.com>
2026-05-14 12:29:10 +0200

    Task 5

--- semestr2/app/Task5/Order.php
+++ semestr2/app/Task5/Order.php
@@ -6,23 +6,43 @@ namespace App\Task5;
 
 class Order
 {
+    private const FREE_SHIPPING_THRESHOLD = 15000;
+    private const SHIPPING_COST = 1500;
+
     private array $items = [];
 
+    public function __construct(
+        private string $number,
+    ) {
+    }
+
     public function addItem(OrderItem $item): void
-    {}
+    {
+        $this->items[] = $item;
+    }
 
     public function getShippingCost(): int
     {
-        return 0;
+        if ($this->calculateItemsTotal() >= self::FREE_SHIPPING_THRESHOLD) {
+            return 0;
+        }
+
+        return self::SHIPPING_COST;
     }
 
     public function calculateItemsTotal(): int
     {
-        return 0;
+        $total = 0;
+
+        foreach ($this->items as $item) {
+            $total += $item->getTotalPrice();
+        }
+
+        return $total;
     }
 
     public function calculateGrandTotal(): int
     {
-        return 0;
+        return $this->calculateItemsTotal() + $this->getShippingCost();
     }
 }
