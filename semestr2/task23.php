6047bb122a1d8cd126d79d3e4c406be3d1a69bc0
K B <kbolenbach@gmail.com>
2026-05-18 21:21:48 +0200

    Zadanie 23

--- semestr2/task23.php
+++ semestr2/task23.php
@@ -1,3 +1,31 @@
 <?php
 
-echo "TODO";
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpClient\HttpClient;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/.env');
+
+$url = $_ENV['API_URL'];
+$token = $_ENV['API_TOKEN'];
+
+$aboutpages = 'webapi/rest/aboutpages';
+
+$endpoint = $url . $aboutpages;
+
+$client = HttpClient::create();
+$response = $client->request('GET', $endpoint, [
+    'headers' => [
+        'Authorization' => 'Bearer ' . $token,
+    ],
+    'timeout' => 3,
+]);
+
+$content = $response->getContent();
+$data = json_decode($content);
+
+foreach($data->list as $page) {
+    echo $page->name . PHP_EOL . "</br>";
+}
