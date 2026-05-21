c8da4f3c3fe14b9123c64bf365b05b8223e3a39f
pwalczak418 <pwalczak418@gmail.com>
2026-05-21 14:25:34 +0200

    Zadanie 25

--- semestr2/task25.php
+++ semestr2/task25.php
@@ -1,3 +1,35 @@
 <?php
 
-echo "TODO";
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpClient\HttpClient;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__."/.env");
+
+$url = $_ENV['API_URL'];
+$token = $_ENV['API_TOKEN'];
+
+if (! $_GET["id"]) {
+    echo "Należy podać parametr ID";
+    die();
+}
+
+$page = $_GET["id"];
+
+$client = HttpClient::create();
+$response = $client->request('DELETE', $url . "aboutpages/" . $page, [
+    'headers' => [
+        'Authorization' => 'Bearer ' . $token,
+    ],
+    'timeout' => 5,
+]);
+
+$statusCode = $response->getStatusCode();
+
+if ($statusCode == 200) {
+    echo "Poprawnie usunięto stronę informacyjną";
+} else {
+    echo "Wystąpił problem z usunięciem strony informacyjnej, najprawdopodobniej nie istnieje strona z takim ID";
+}
