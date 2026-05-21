30780cfb2d5a41ef6bcc64d47ee0dc460e1ab0a8
pwalczak418 <pwalczak418@gmail.com>
2026-05-21 13:50:39 +0200

    Zadanie 24

--- semestr2/task24.php
+++ semestr2/task24.php
@@ -1,3 +1,26 @@
 <?php
 
-echo "TODO";
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpClient\HttpClient;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__."/.env");
+
+$url = $_ENV['API_URL'];
+$token = $_ENV['API_TOKEN'];
+
+$body = ['name' => 'test', 'lang_id' => 1, 'content' => 'Tu jest treść strony testowej'];
+$bodyJson = json_encode($body);
+
+$client = HttpClient::create();
+$response = $client->request('POST', $url . "aboutpages", [
+    'headers' => [
+        'Authorization' => 'Bearer ' . $token,
+    ],
+    'timeout' => 5,
+    'body' => $bodyJson
+]);
+
+echo "Odpowiedź z API: " . $response->getContent();
