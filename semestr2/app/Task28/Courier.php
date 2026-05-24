c89788b23bd3b387142fc411a11ad3c85253ebcd
pwalczak418 <pwalczak418@gmail.com>
2026-05-24 21:47:38 +0200

    Zadanie 28

--- semestr2/app/Task28/Courier.php
+++ semestr2/app/Task28/Courier.php
@@ -2,4 +2,29 @@
 
 declare(strict_types=1);
 
-// TODO
+namespace App\Task28;
+
+class Courier extends ShippingMethod {
+    public function __construct(
+        $baseCost
+    ) {
+        parent::__construct($baseCost);
+    }
+
+    public function calculateCost(Order $order): int 
+    {
+        if ($order->getWeight() > 10000) {
+            $overweight = ($order->getWeight() - 10000);
+            $overweightKilo = (int) ($overweight / 1000);
+            $modulo = $overweight % 1000;
+            if ($modulo) {
+                $surcharge = ($overweightKilo * 500) + 500;
+            } else {
+                $surcharge = ($overweightKilo * 500);
+            }
+            return $this->baseCost + $surcharge;
+        } else {
+            return $this->baseCost;
+        }
+    }
+}
--- semestr2/app/Task28/ParcelLocker.php
+++ semestr2/app/Task28/ParcelLocker.php
@@ -2,4 +2,24 @@
 
 declare(strict_types=1);
 
-// TODO
+namespace App\Task28;
+
+class ParcelLocker extends ShippingMethod {
+    public function __construct(
+        $baseCost
+    ) {
+        parent::__construct($baseCost);
+    }
+
+    public function calculateCost(Order $order): int 
+    {
+        if ($order->getWeight() > 25000) {
+            throw new \InvalidArgumentException('Przesyłka niemożliwa do realizacji za pomocą tej formy dostawy');
+        }
+        if ($order->getTotalCost() >= 50000) {
+            return 0;
+        } else {
+            return $this->baseCost;
+        }
+    }
+}
