b10a8a43c6c361ed9608062a16f2be89bf1687fc
Kinga Horniak <kitfa6@gmail.com>
2026-05-25 21:59:25 +0200

    Zadanie 6

--- semestr2/task6.php
+++ semestr2/task6.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "TODO";
+
+require 'vendor/autoload.php';
+
+use App\Task6\OrderProcessor;
+use App\Task6\Payment\PaymentGateway as Payment;
+use App\Task6\Shipping\ShippingService as Shipping;
+
+
+$paymentGateway = new Payment("Blik");
+$shippingService = new Shipping("DPD");
+
+$processor = new OrderProcessor($paymentGateway, $shippingService);
+$result = $processor->processOrder('ORD-2026-001', 299.99, 1.5, 'ul. Pawia 9, Kraków');
+print_r($result);
