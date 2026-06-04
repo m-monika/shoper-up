35eeed3fdc75e87c6375ba5c41514deb8b89288e
Kinga Horniak <kitfa6@gmail.com>
2026-06-04 11:14:38 +0200

    Zadanie 8

--- semestr2/app/Task8/Basket.php
+++ semestr2/app/Task8/Basket.php
@@ -8,22 +8,43 @@ use App\Task8\Coupons\CouponInterface;
 
 class Basket
 {
+    private array $products = [];
+     private ?CouponInterface $coupon = null;
+
     public function addProduct(Product $product, int $quantity): void
     {
+        foreach($this->products as &$item){
+            if ($item['product']->getName() == $product->getName()){
+                $item['quantity'] += $quantity;
+                return;
+            }
+        }
+
+        $this->products[] = [
+            'product' => $product,
+            'quantity' => $quantity,
+        ];
     }
 
     public function applyCoupon(CouponInterface $coupon): void
     {
+        $this->coupon = $coupon;
     }
+    
 
     public function getProducts(): array
     {
-        return [];
+        return $this->products;
+       
     }
 
     public function getTotalWithoutDiscount(): float
     {
         $total = 0.0;
+
+        foreach($this->products as $item){
+           $total += $item['product']->getPrice() * $item['quantity'];
+        }
 
         return $total;
     }
@@ -31,6 +52,10 @@ class Basket
     public function getTotalWithDiscount(): float
     {
         $total = $this->getTotalWithoutDiscount();
+
+        if($this->coupon !== null){
+            return $this->coupon->applyDiscount($total);
+        }
 
         return $total;
     }
