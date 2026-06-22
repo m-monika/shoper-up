adf14518169ce05a563a1648ad1702ee3eed1f73
kapakrystian <kapakrystian@gmail.com>
2026-06-22 19:12:21 +0200

    Zadanie 28

--- semestr2/app/Task28/ParcelLocker.php
+++ semestr2/app/Task28/ParcelLocker.php
@@ -2,4 +2,24 @@
 
 declare(strict_types=1);
 
-// TODO
+namespace App\Task28;
+
+
+class ParcelLocker extends ShippingMethod
+{
+    public function calculateCost(Order $order): int
+    {
+        $orderWeight = $order->getWeight();
+        $orderCost = $order->getTotalCost();
+
+        if ($orderWeight > 25000) {
+            throw new \InvalidArgumentException('Waga zbyt duża dla wybranej formy dostawy');
+        }
+
+        if ($orderCost >= 50000) {
+            return 0;
+        }
+
+        return $this->baseCost;
+    }
+}
