f764cfda95a3d81b9edfce0fc56baa2faef4aad6
Daniel Kania <kania.daniel98@gmail.com>
2026-06-24 20:49:09 +0200

    Zadanie 28

--- semestr2/app/Task28/ParcelLocker.php
+++ semestr2/app/Task28/ParcelLocker.php
@@ -2,4 +2,20 @@
 
 declare(strict_types=1);
 
-// TODO
+namespace App\Task28;
+
+class ParcelLocker extends ShippingMethod
+{
+    public function calculateCost(Order $order): int
+    {
+        if ($order->getWeight() > 25000) {
+            throw new \InvalidArgumentException('Zamówienie jest za ciężkie.');
+        }
+
+        if ($order->getTotalCost() >= 50000) {
+            return 0;
+        }
+
+        return $this->baseCost;
+    }
+}
